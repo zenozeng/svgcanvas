298cb72b74024b74c08935499175540740a2d5de
Zeno Zeng <zenoofzeng@gmail.com>
2022-03-09 22:58:22 +0800

    test: update text test

--- test/tests/text.js
+++ test/tests/text.js
@@ -1,8 +1,5 @@
 export default function text(ctx) {
-    ctx.font = "normal 36px Times";
+    ctx.font = "normal 120px Arial";
     ctx.fillStyle = "#000000";
-    ctx.fillText("A Text Example", 50, 50);
-    ctx.font = "normal 36px Arial";
-    ctx.strokeStyle = "#000000";
-    ctx.strokeText("A Text Example", 50, 90);
+    ctx.fillText("Hello", 0, 200);
 };
