c192c00864533e23c5584418a81072f24c0dfed6
Zeno Zeng <zenoofzeng@gmail.com>
2022-03-09 22:33:31 +0800

    test: diff pixels

--- test/rendering.test.js
+++ test/rendering.test.js
@@ -1,4 +1,5 @@
-import {Element} from '../index'
+import { Element } from '../index'
+import { expect } from 'chai'
 import arc from './tests/arc'
 import arcTo from './tests/arcTo'
 import arcTo2 from './tests/arcTo2'
@@ -51,6 +52,35 @@ class RenderingTester {
         const height = 500;
         const canvas = document.createElement('canvas');
         const svgcanvas = new Element();
+
+        [canvas, svgcanvas].forEach((canvas) => {
+            canvas.width = width
+            canvas.height = height
+            const ctx = canvas.getContext('2d')
+            this.fn(ctx)
+        })
+
+        // Pixels
+        const svg = svgcanvas.toDataURL("image/svg+xml");
+        const svgImage = await new Promise((resolve) => {
+            var svgImage = new Image();
+            svgImage.onload = function () {
+                resolve(svgImage)
+            }
+            svgImage.src = svg;
+        })
+        const svgPixels = this.getPixels(svgImage);
+        const canvasPixels = this.getPixels(canvas);
+        const diffPixels = this.diffPixels(svgPixels, canvasPixels);
+        const removeThinLinesPixels = this.removeThinLines(this.removeThinLines(diffPixels));
+        const count = Math.max(this.countPixels(svgPixels), this.countPixels(canvasPixels));
+        const diffCount = this.countPixels(diffPixels);
+        console.log({ count, diffCount })
+        if (count === 0 && diffCount === 0) {
+            return 0
+        }
+        const diffRate = diffCount / count;
+        return diffRate;
     }
 
     getPixels(image) {
@@ -62,7 +92,19 @@ class RenderingTester {
         const ctx = canvas.getContext('2d');
         ctx.drawImage(image, 0, 0, width, height);
         return ctx.getImageData(0, 0, width, height);
-    }    
+    }
+
+    // count non transparent pixels
+    countPixels(imgData) {
+        var count = 0;
+        for (var i = 3; i < imgData.data.length; i += 4) {
+            if (imgData.data[i] > 0) {
+                count++;
+            }
+        }
+        return count;
+    };
+
 
     diffPixels(imgData1, imgData2) {
         const canvas = document.createElement('canvas');
@@ -71,24 +113,79 @@ class RenderingTester {
         const diffImgData = canvas.getContext('2d').getImageData(0, 0, width, height);
         let $this = this;
         for (var i = 0; i < imgData1.data.length; i += 4) {
-            var indexes = [i, i+1, i+2, i+3];
-            indexes.forEach(function(i) {
+            var indexes = [i, i + 1, i + 2, i + 3];
+            indexes.forEach(function (i) {
                 diffImgData.data[i] = 0;
             });
-            if(indexes.some(function(i) {
+            if (indexes.some(function (i) {
                 return Math.abs(imgData1.data[i] - imgData2.data[i]) > $this.maxPixelDiff;
             })) {
-                diffImgData.data[i+3] = 255; // set black
+                diffImgData.data[i + 3] = 255; // set black
             }
         }
         return diffImgData;
-    }    
+    }
+
+    removeThinLines(imageData) {
+        const canvas = document.createElement('canvas');
+        const width = 100 * config.pixelDensity;
+        const height = 100 * config.pixelDensity;
+        canvas.width = width;
+        canvas.height = height;
+        const ctx = canvas.getContext('2d');
+        ctx.putImageData(imageData, 0, 0);
+        var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        var imgDataCopy = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        var getPixelIndex = function (x, y) {
+            return (y * width + x) * 4 + 3;
+        };
+
+        var getPixel = function (x, y) {
+            var alphaIndex = getPixelIndex(x, y);
+            return imgDataCopy.data[alphaIndex];
+        };
+
+        var setPixel = function (x, y, value) {
+            imgData.data[getPixelIndex(x, y)] = value;
+        };
+
+        for (var x = 1; x < width - 1; x++) {
+            for (var y = 1; y < height - 1; y++) {
+                if (getPixel(x, y) == 0) {
+                    continue; // ignore transparents
+                }
+                var links = [
+                    { x: x - 1, y: y - 1 },
+                    { x: x, y: y - 1 },
+                    { x: x + 1, y: y - 1 },
+                    { x: x - 1, y: y },
+                    { x: x + 1, y: y },
+                    { x: x - 1, y: y + 1 },
+                    { x: x, y: y + 1 },
+                    { x: x + 1, y: y + 1 }
+                ].map(function (p) {
+                    return getPixel(p.x, p.y);
+                }).filter(function (val) {
+                    return val > 0; // not transparent?
+                }).length;
+
+                if (links < 5) { // is a thin line
+                    setPixel(x, y, 0); // make it transparent
+                }
+            }
+        }
+        return imgData;
+    }
 }
 
 describe('RenderTest', () => {
     for (let fn of Object.keys(tests)) {
         it(`should render same results for ${fn}`, async () => {
-
+            const tester = new RenderingTester(tests[fn]);
+            const diffRate = await tester.test();
+            console.log({ diffRate, fn });
+            expect(diffRate).to.lessThan(0.05);
         })
     }
 })
