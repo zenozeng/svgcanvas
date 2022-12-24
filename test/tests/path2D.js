fd024674c0f3c52f436757ea737cfd02d855aa9f
k1w1 <chris@aha.io>
2022-12-24 14:15:14 -0800

    Added clipping test

--- test/tests/path2D.js
+++ test/tests/path2D.js
@@ -1,29 +1,48 @@
 function makePath(ctx, arg) {
-    if (ctx.createPath) {
-        return ctx.createPath(arg);
-    } else {
-        return new Path2D(arg);
-    }
+  if (ctx.createPath) {
+    return ctx.createPath(arg);
+  } else {
+    return new Path2D(arg);
+  }
 }
 
 export default function path2D(ctx) {
-    const path1 = makePath(ctx, `M 230 80
+  const path1 = makePath(
+    ctx,
+    `M 230 80
         A 45 45, 0, 1, 0, 275 125
-        L 275 80 Z`);
+        L 275 80 Z`
+  );
 
-    ctx.strokeStyle = 'red';
-    ctx.stroke(path1);
-    ctx.fillStyle = 'grey';
-    ctx.fill(path1);
+  ctx.save();
+  ctx.strokeStyle = "red";
+  ctx.stroke(path1);
+  ctx.fillStyle = "grey";
+  ctx.fill(path1);
 
-    ctx.translate(10, 25);
-    ctx.lineWidth = 10;
-    ctx.stroke(path1);
+  ctx.translate(10, 25);
+  ctx.lineWidth = 10;
+  ctx.stroke(path1);
 
-    ctx.rotate(Math.PI / 4);
-    ctx.scale(1.5, 1.5);
-    ctx.translate(10, 25);
+  ctx.rotate(Math.PI / 4);
+  ctx.scale(1.5, 1.5);
+  ctx.translate(10, 25);
 
-    ctx.strokeStyle = 'blue';
-    ctx.stroke(path1);
- };
+  ctx.strokeStyle = "blue";
+  ctx.stroke(path1);
+  ctx.restore();
+
+  ctx.save();
+  // Stroke and fill the same path.
+  ctx.beginPath();
+  ctx.rect(10, 10, 40, 20);
+  ctx.save();
+  ctx.strokeStyle = "red";
+  ctx.stroke();
+  ctx.restore();
+  ctx.save();
+  ctx.fillStyle = "green";
+  ctx.fill();
+  ctx.restore();
+  ctx.restore();
+}
